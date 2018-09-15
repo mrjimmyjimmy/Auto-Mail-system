26ccafd350e7cd8dceb6ce6ba274648b94878290
zack0518 <774726628@qq.com>
2018-09-15 17:58:42 +1000

    加了备注

--- Mail/exceptions/FragileItemCannotDeliverException.java
+++ Mail/exceptions/FragileItemCannotDeliverException.java
@@ -1,5 +1,9 @@
 package exceptions;
 
+/**
+ * An exception thrown when all robot unable to fragile item
+ */
+
 public class FragileItemCannotDeliverException extends Throwable {
 	public FragileItemCannotDeliverException(){
 		super("Have Fragile item, But no careful robot to deliver, Please adjust robot types");
--- Mail/exceptions/HeavyItemCannotDeliverException.java
+++ Mail/exceptions/HeavyItemCannotDeliverException.java
@@ -1,5 +1,9 @@
 package exceptions;
 
+/**
+ * An exception thrown when all robot unable to deliver the item that is heavier than 2000
+ */
+
 public class HeavyItemCannotDeliverException extends Throwable {
     public HeavyItemCannotDeliverException(){
         super("Have item that is heavier than 2000 grams, But there are only week robot avaiable, Please adjust robot types");
